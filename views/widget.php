cdd1987ae05e12984b49f47e7fbfef5122383c8d
NeutralX2 <neutralx2@gmail.com>
2026-08-10 19:37:43 -0400

    Update widget.php
    Prevent possible XSS

--- views/widget.php
+++ views/widget.php
@@ -4,17 +4,17 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 <div class="table">
     <div class="row">
 		<div class="column" style="padding: 5px;">
-			<a href="<?= $profile->getProfileUrl() ?>" target="_blank" >
-				<img class="avatar bg <?= $profile->getPersonaState() ?>" src="<?= $profile->getAvatarMedium() ?>" title="<?= $profile->getPersonaName() ?>" />
+			<a href="<?= esc_url($profile->getProfileUrl()) ?>" target="_blank" >
+				<img class="avatar bg <?= esc_attr($profile->getPersonaState()) ?>" src="<?= esc_url($profile->getAvatarMedium()) ?>" title="<?= esc_attr($profile->getPersonaName()) ?>" />
 			</a>
 		</div>
 		<div class="column" style="padding: 5px;">
 			<p>
-				<a class="fg <?= $profile->getPersonaState() ?>" href="<?= $profile->getProfileUrl() ?>" target="_blank">
-					<?= $profile->getPersonaName() ?> (<?= $profile->getPersonaState() ?>)
+				<a class="fg <?= esc_attr($profile->getPersonaState()) ?>" href="<?= esc_url($profile->getProfileUrl()) ?>" target="_blank">
+					<?= esc_html($profile->getPersonaName()) ?> (<?= esc_html($profile->getPersonaState()) ?>)
 				</a>
 				<br />
-				Since <?= $profile->getTimeCreated('m/d/Y') ?>
+				Since <?= esc_html($profile->getTimeCreated('m/d/Y')) ?>
 			</p>
 		</div>
     </div>
@@ -26,12 +26,12 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 	<div>
 		<?php $pgame = $games->getGameByAppId($profile->getGameId()) ?>
 		<?php if ($pgame) : ?>
-			<a href="<?= $pgame->getLink() ?>" target="_blank" title="<?= $pgame->getName() ?>">
-				<img src="<?= $pgame->getHeader() ?>" alt="<?= $pgame->getName() ?>"/>
+			<a href="<?= esc_url($pgame->getLink()) ?>" target="_blank" title="<?= esc_attr($pgame->getName()) ?>">
+				<img src="<?= esc_url($pgame->getHeader()) ?>" alt="<?= esc_attr($pgame->getName()) ?>"/>
 			</a>
 		<?php else : ?>
 			<div class="message">
-				<p><b><?= $profile->getGameExtraInfo() ?></b></p>
+				<p><b><?= esc_html($profile->getGameExtraInfo()) ?></b></p>
 			</div>
 		<?php endif; ?>
 	</div>
@@ -42,19 +42,18 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 	<div class="table">
 		<?php foreach ($games->getRecentPlayedGames($count) as $game) : ?>
 			<div class="row">
-				<a href="<?= $game->getLink() ?>" target="_blank">
+				<a href="<?= esc_url($game->getLink()) ?>" target="_blank">
 					<div class="column">
-						<img src="<?= $game->getImage() ?>" title="<?= $game->getName() ?>" />
+						<img src="<?= esc_url($game->getImage()) ?>" title="<?= esc_attr($game->getName()) ?>" />
 					</div>
 					<div class="column" style="padding: 0px 5px 0px 5px">
-						<p class="fg <?= $profile->getPersonaState() ?>">&ndash;</p>
+						<p class="fg <?= esc_attr($profile->getPersonaState()) ?>">&ndash;</p>
 					</div>
 					<div class="column">
-						<p class="fg <?= $profile->getPersonaState() ?>"> <?= $game->getName() ?></p>
+						<p class="fg <?= esc_attr($profile->getPersonaState()) ?>"> <?= esc_html($game->getName()) ?></p>
 					</div>
 				</a>
 			</div>
 		<?php endforeach; ?>
 	</div>
 <?php endif; ?>
-
